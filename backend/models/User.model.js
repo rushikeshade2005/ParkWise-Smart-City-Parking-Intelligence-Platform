c145978d358094dfbbc8540911408cb9928d74c4
Rushikesh <rushikeshade2005@gmail.com>
2026-02-23 20:29:12 +0530

    Add User model with roles and secure password hashing

---
+++ backend/models/User.model.js
@@ -0,0 +1,73 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+
+    phone: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+      select: false, // password will not be returned by default
+    },
+
+    role: {
+      type: String,
+      enum: ["USER", "PARKING_ADMIN", "CITY_ADMIN"],
+      default: "USER",
+    },
+
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+
+    lastLogin: {
+      type: Date,
+    },
+  },
+  {
+    timestamps: true, // createdAt & updatedAt
+  }
+);
+
+/**
+ * 🔐 Hash password before saving user
+ */
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+/**
+ * 🔑 Compare entered password with hashed password
+ */
+userSchema.methods.comparePassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
